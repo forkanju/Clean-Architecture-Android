0c6cfb011ccf1a7a17a51f9866391eb620a9bd12
forkanju <46061345+forkanju@users.noreply.github.com>
2023-10-26 14:56:40 +0600

    Initial commit

---
+++ app/src/main/java/com/lj/app/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.lj.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+const val PURPLE_200 = 0xFFBB86FC
+const val PURPLE_500 = 0xFF6200EE
+const val PURPLE_700 = 0xFF3700B3
+const val TEAL_200 = 0xFF03DAC5
+
+val Purple200 = Color(PURPLE_200)
+val Purple500 = Color(PURPLE_500)
+val Purple700 = Color(PURPLE_700)
+val Teal200 = Color(TEAL_200)
